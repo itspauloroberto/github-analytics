6b9fd888645543ce6e8941ead080f6a326b1860b
itspauloroberto <itspauloroberto@gmail.com>
2021-06-18 13:45:35 -0300

    feat: add axios package and config instance for api requests

---
+++ src/api/index.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default axios.create({
+  baseURL: process.env.REACT_APP_GITHUB_API || '',
+  headers: {
+      Accept: 'application/vnd.github.v3+json'
+  }
+});
